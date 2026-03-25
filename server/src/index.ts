5f597c2d3513c64c2bc036d1418fc75e1c20dad7
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2026-03-25 12:17:44 +0700

    feat: add express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hallo");
+import express from "express";
+
+const app = express();
+
+app.get("/status", (req, res) => {
+  return res.status(200).json({
+    health: true,
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server run at port ${port}`);
+});
